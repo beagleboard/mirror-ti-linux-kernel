aa9f8489fd8ff04eeefa6690101b90817f128dc5
Judith Mendez <jm@ti.com>
2026-02-02 13:59:27 -0600

    PENDING: mmc: sdhci_am654: Add SDHCI_QUIRK_CAP_CLOCK_BASE_BROKEN quirk
    Add quirk SDHCI_QUIRK_CAP_CLOCK_BASE_BROKEN to devices with DLL_PRESENT
    so that DLL configuration can be determined using the actual clock to the
    MMC card instead of the theoretical max clock.
    
    Signed-off-by: Judith Mendez <jm@ti.com>
    Tested-by: Moteen Shah <m-shah@ti.com>

--- drivers/mmc/host/sdhci_am654.c
+++ drivers/mmc/host/sdhci_am654.c
@@ -669,7 +669,8 @@ static const struct sdhci_ops sdhci_j721e_8bit_ops = {
 
 static const struct sdhci_pltfm_data sdhci_j721e_8bit_pdata = {
 	.ops = &sdhci_j721e_8bit_ops,
-	.quirks = SDHCI_QUIRK_MULTIBLOCK_READ_ACMD12,
+	.quirks = SDHCI_QUIRK_MULTIBLOCK_READ_ACMD12 |
+		  SDHCI_QUIRK_CAP_CLOCK_BASE_BROKEN,
 	.quirks2 = SDHCI_QUIRK2_PRESET_VALUE_BROKEN |
 		   SDHCI_QUIRK2_DISABLE_HW_TIMEOUT,
 };
