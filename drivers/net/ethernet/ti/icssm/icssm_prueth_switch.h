80db325dcb19e2f3daca2c078a877c0a5715d8aa
Roger Quadros <rogerq@ti.com>
2026-02-18 16:28:08 +0530

    PENDING: net: ti: prueth: Adds TX optimization for HSR/PRP
    This patch adds changes related to optimization of HSR driver by
    avoiding duplication of packets which need to be sent on both the ports.
    
    To achieve this we have merged TX queues of both the ports and the PRUs
    will take care of synchronization between cores while transmitting the
    packets.
    
    With this changes driver will write packet into queue only one time which
    avoids double copy and let firmware duplicate the packet and send on both
    the ports.
    
    Handled cases like transmit on both ports and only one port using BD's.
    
    Reviewed-by: pmohan <pmohan@couthit.com>
    Signed-off-by: Roger Quadros <rogerq@ti.com>
    Signed-off-by: Andrew F. Davis <afd@ti.com>
    Signed-off-by: Parvathi Pudi <parvathi@couthit.com>
    Signed-off-by: MD Danish Anwar <danishanwar@ti.com>

--- drivers/net/ethernet/ti/icssm/icssm_prueth_switch.h
+++ drivers/net/ethernet/ti/icssm/icssm_prueth_switch.h
@@ -17,6 +17,8 @@ u8 icssm_prueth_sw_get_stp_state(struct prueth *prueth,
 				 enum prueth_port port);
 
 extern const struct prueth_queue_info sw_queue_infos[][4];
+/* HSR PRP Tx Optimization: Separate Tx queue structure for HSR PRP */
+extern const struct prueth_queue_info lre_queue_infos[][4];
 
 void icssm_prueth_sw_fdb_tbl_init(struct prueth *prueth);
 int icssm_prueth_sw_init_fdb_table(struct prueth *prueth);
--- net/hsr/hsr_forward.c
+++ net/hsr/hsr_forward.c
@@ -412,6 +412,7 @@ struct sk_buff *hsr_create_tagged_frame(struct hsr_frame_info *frame,
 		sred->lsdu_size = s & 0xfff;
 		sred->pathid = (s >> 12) & 0xf;
 		sred->seqnr = hsr_get_skb_sequence_nr(skb);
+		frame->is_l2ptp = true;
 	}
 
 	return skb;
@@ -622,6 +623,7 @@ hsr_directed_tx_ports(struct hsr_frame_info *frame)
 static void hsr_forward_do(struct hsr_frame_info *frame)
 {
 	unsigned int dir_ports = 0;
+	int skip_tx_duplicate = -1;
 	struct hsr_port *port;
 	struct sk_buff *skb;
 	bool sent = false;
@@ -644,6 +646,9 @@ static void hsr_forward_do(struct hsr_frame_info *frame)
 		 * one port.
 		 */
 		if ((port->dev->features & NETIF_F_HW_HSR_DUP) && sent)
+			continue;
+
+		if (port->type == HSR_PT_SLAVE_B && skip_tx_duplicate == 0)
 			continue;
 
 		/* Don't send frame over port where it has been sent before.
@@ -686,14 +691,19 @@ static void hsr_forward_do(struct hsr_frame_info *frame)
 			continue;
 		}
 
+		if (frame->is_l2ptp)
+			skip_tx_duplicate = -1;
+
 		skb->dev = port->dev;
 		if (port->type == HSR_PT_MASTER) {
 			hsr_deliver_master(skb, port, frame->node_src);
 		} else {
-			if (!hsr_xmit(skb, port, frame))
+			if (skip_tx_duplicate != 0) {
+				skip_tx_duplicate = hsr_xmit(skb, port, frame);
 				if (port->type == HSR_PT_SLAVE_A ||
 				    port->type == HSR_PT_SLAVE_B)
 					sent = true;
+			}
 		}
 	}
 }
